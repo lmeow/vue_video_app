ee2d04aeeb2ed7a19c0a7bbe3639cdc4c2c6ffa6
Maja Frankiewicz <majafrank@live.se>
2020-08-28 16:33:33 +0200

    first commit

---
+++ src/components/VideoList.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+    <VideoListItem v-for="video in videos" :key="video"></VideoListItem>
+</ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+
+export default {
+    name: "VideoList",
+    props: ['videos'],
+    compontents: {
+        VideoListItem
+    }
+}
+</script>
+
+<style>
+
+</style>
